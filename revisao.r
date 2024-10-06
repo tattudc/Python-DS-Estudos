e8350823960bdb69ae3741b71c445df1065ecddd
tattudc <tarcisiodantasrn@gmail.com>
2024-10-05 22:40:52 -0300

    update revisao.r

--- revisao.r
+++ revisao.r
@@ -3,3 +3,29 @@ xcaractere = 'a'
 xinteger = 2
 print(paste('caractere', xcaractere))
 print(paste('Inteiro', xinteger))
+
+#operacoes aritméticas 
+soma <- 5 + 3 
+soma
+produto <- 5 * 3 
+produto
+sub <- 5-3
+sub
+div <- 4/2
+div
+pot <- 2^3 
+pot
+resto <- 10 %% 3
+resto
+
+#operadores relacionais
+x <- 10
+y <- 5
+x > y 
+x == y
+x != y
+x < y
+
+#funcoes basicas
+sqrt(16)  
+mean(c(1, 2, 3))
